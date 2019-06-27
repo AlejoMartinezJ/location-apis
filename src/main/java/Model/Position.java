2fd359a8bbb4c4433f960f5e8c15c628c4048575
AlejoMartinezJ <48266391+AlejoMartinezJ@users.noreply.github.com>
2019-06-26 23:31:00 -0400

    Done

--- src/main/java/Model/Position.java
+++ src/main/java/Model/Position.java
@@ -7,7 +7,7 @@ public class Position {
     private String lng;
     private Integer order;
     private String placeName;
-    private boolean status;
+    private boolean locationStatus;
 
     public Position(Integer locationid, String lat, String lng, Integer order, String name) {
         this.locationid = locationid;
